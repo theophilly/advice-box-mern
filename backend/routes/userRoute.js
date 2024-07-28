2276fb447e8f551e24581b7f310d5754e2a8ab4d
theodasa <theophilly20@gmail.com>
2024-07-28 10:38:42 +0100

    fix: s3 issue

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,33 +1,42 @@
-import express from 'express';
-import aws from 'aws-sdk';
-import { validationResult } from 'express-validator';
-import jwt from 'jsonwebtoken';
-import multer from 'multer';
-import multerS3 from 'multer-s3';
-import shortid from 'shortid';
+import express from "express";
+import { config } from "dotenv";
+import aws from "aws-sdk";
+import { validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+import multer from "multer";
+import multerS3 from "multer-s3";
+import shortid from "shortid";
+import { S3Client, DeleteObjectCommand } from "@aws-sdk/client-s3";
 
-import User from '../models/userModel.js';
-import { signUpValidator } from '../validators/index.js';
-import { ensureLogin, userLoginMiddleware } from '../middlewares/auth.js';
+const s3 = new S3Client({
+  region: "us-east-2",
+  credentials: {
+    accessKeyId: config().parsed.AWS_ACCESS_KEY_ID,
+    secretAccessKey: config().parsed.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+import User from "../models/userModel.js";
+import { signUpValidator } from "../validators/index.js";
+import { ensureLogin, userLoginMiddleware } from "../middlewares/auth.js";
 
 const route = express.Router();
-aws.config.update({ region: 'us-west-2' });
-const s3 = new aws.S3({ apiVersion: '2006-03-01' });
+aws.config.update({ region: "us-east-2" });
 
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: 'advicebox',
+    bucket: "advicebox",
     metadata: function (req, file, cb) {
-      cb(null, { fieldName: 'file' });
+      cb(null, { fieldName: "file" });
     },
     key: function (req, file, cb) {
-      cb(null, shortid.generate() + '-' + file.originalname);
+      cb(null, shortid.generate() + "-" + file.originalname);
     },
   }),
 });
 
-route.post('/signin', signUpValidator, async (req, res) => {
+route.post("/signin", signUpValidator, async (req, res) => {
   const result = validationResult(req);
 
   if (!result.isEmpty()) {
@@ -51,37 +60,37 @@ route.post('/signin', signUpValidator, async (req, res) => {
             profilePicture: user.profilePicture,
           },
           process.env.JWT_SECRET,
-          { expiresIn: '4h' }
+          { expiresIn: "4h" }
         );
-        res.cookie('token', token, { expiresIn: '4h' });
+        res.cookie("token", token, { expiresIn: "4h" });
         return res.status(200).json({ token, user });
       } else {
-        return res.status(400).json({ message: 'incorrect password' });
+        return res.status(400).json({ message: "incorrect password" });
       }
     } else {
-      return res.status(400).json({ message: 'User does not exist' });
+      return res.status(400).json({ message: "User does not exist" });
     }
   } catch (error) {
     return res.status(400).json({ error: error.message });
   }
 });
 
-route.post('/signout', async (req, res) => {
+route.post("/signout", async (req, res) => {
   res.clearCookie();
-  return res.status(200).json({ message: 'You have logout successfully' });
+  return res.status(200).json({ message: "You have logout successfully" });
 });
-route.get('/getuser/:userName', async (req, res) => {
+route.get("/getuser/:userName", async (req, res) => {
   const userName = req.params.userName;
 
   try {
     const user = await User.findOne({ userName }).select(
-      'userName profilePicture about'
+      "userName profilePicture about"
     );
 
     if (user) {
       return res.status(200).json({ user });
     } else {
-      return res.status(400).json({ message: 'user' });
+      return res.status(400).json({ message: "user" });
     }
   } catch (error) {
     res.status(400).json({ error: error });
@@ -91,10 +100,10 @@ route.get('/getuser/:userName', async (req, res) => {
 // upload.single('file'),
 
 route.post(
-  '/updateuser',
+  "/updateuser",
   ensureLogin,
   userLoginMiddleware,
-  upload.single('file'),
+  upload.single("file"),
   async (req, res) => {
     console.log(req.file);
 
@@ -104,7 +113,7 @@ route.post(
       user = await User.findOne({ _id: req.user.id });
     } catch (error) {
       return res.status(400).json({
-        message: 'something went wrong',
+        message: "something went wrong",
         error: error.message,
       });
     }
@@ -120,35 +129,33 @@ route.post(
         await user.save();
       } catch (error) {
         return res.status(400).json({
-          message: 'something went wrong',
+          message: "something went wrong",
           error: error.message,
         });
       }
 
-      if (deletePicture !== '' && deletePicture !== req.file.key) {
+      if (deletePicture !== "" && deletePicture !== req.file.key) {
         try {
-          await s3.deleteObject(
-            { Bucket: 'advicebox', Key: deletePicture },
-            function (err, data) {
-              if (err) console.log(err, err.stack);
-              // an error occurred
-              else console.log(data); //   successful response
-            }
-          );
+          const command = new DeleteObjectCommand({
+            Bucket: "advicebox",
+            Key: deletePicture,
+          });
+          await s3.send(command);
         } catch (err) {
+          console.log(err);
           return res.status(400).json({
-            message: 'something went wrong',
+            message: "something went wrong",
             error: err.message,
           });
         }
       }
     }
 
-    return res.status(200).json({ user, message: 'updated successfully' });
+    return res.status(200).json({ user, message: "updated successfully" });
   }
 );
 
-route.post('/signup', signUpValidator, async (req, res) => {
+route.post("/signup", signUpValidator, async (req, res) => {
   const result = validationResult(req);
 
   if (!result.isEmpty()) {
@@ -165,7 +172,7 @@ route.post('/signup', signUpValidator, async (req, res) => {
     const user = await User.findOne({ userName });
 
     if (user) {
-      return res.status(400).json({ message: 'Username already exist' });
+      return res.status(400).json({ message: "Username already exist" });
     }
 
     const newUser = await new User({ userName, password });
@@ -179,9 +186,9 @@ route.post('/signup', signUpValidator, async (req, res) => {
             profilePicture: newuser.profilePicture,
           },
           process.env.JWT_SECRET,
-          { expiresIn: '4h' }
+          { expiresIn: "4h" }
         );
-        res.cookie('token', token, { expiresIn: '4h' });
+        res.cookie("token", token, { expiresIn: "4h" });
         return res.status(200).json({ token, user: newuser });
       } else {
         return res.status(400).json({ message: error.message });
