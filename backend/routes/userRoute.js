b10e33553f2887e075d54cdfceda62693dd460a0
theodasa <theophilly20@gmail.com>
2024-07-28 11:56:53 +0100

    fix: s3 issue

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -11,8 +11,8 @@ import { S3Client, DeleteObjectCommand } from "@aws-sdk/client-s3";
 const s3 = new S3Client({
   region: "us-east-2",
   credentials: {
-    accessKeyId: config().parsed.AWS_ACCESS_KEY_ID,
-    secretAccessKey: config().parsed.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
 
